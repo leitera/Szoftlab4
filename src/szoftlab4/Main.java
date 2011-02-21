8a39a5a752f285ed6d26f1706fdc49e3158c1bc2
leitera <leitera@gmail.com>
2011-02-21 16:19:48 +0100

    megcsináltam 3 uj sort

--- src/szoftlab4/Main.java
+++ src/szoftlab4/Main.java
@@ -16,6 +16,9 @@ public class Main {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("asdasd");
+        System.out.println("asfdsfasdf");
+
     }
 
 }
