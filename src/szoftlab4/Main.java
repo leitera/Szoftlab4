2cbc43d4a35766ff9652c302e514a94643a0a513
leitera <leitera@gmail.com>
2011-02-21 16:55:40 +0100

    szuperjo dolgok tortentek

--- src/szoftlab4/Main.java
+++ src/szoftlab4/Main.java
@@ -17,8 +17,11 @@ public class Main {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("asdasd");
-        System.out.println("asfdsfasdf");
-
+        System.out.println("asfdsfasd");
+        System.out.println("asfdsfasf");
+        
+        //hehe
+        
     }
 
 }
