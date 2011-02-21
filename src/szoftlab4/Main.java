372175a3de5703cbce64577da4410b4d5220d956
Gergely Hernesz <herneszg@gmail.com>
2011-02-21 16:58:53 +0100

    Csinaltam bele dolgot

--- src/szoftlab4/Main.java
+++ src/szoftlab4/Main.java
@@ -19,6 +19,8 @@ public class Main {
         System.out.println("asdasd");
         System.out.println("asfdsfasd");
         System.out.println("asfdsfasf");
+	
+	// berakok uj dolgot bele
         
         //hehe
         
